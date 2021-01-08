09c9d0e46e127db5753745ddd1840fee3a60c21e
Krishna vamsi <65479976+krishna-creator@users.noreply.github.com>
2021-01-08 17:45:37 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,18 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0.5; url=index.html">
     <title>Document</title>
 </head>
-<body>
+<body style="display:flex;justify-content:center;padding:50px">
     <?php
-    $nm=$_POST["nm"];
-    $em=$_POST["gm"];
-    $mb=$_POST["mb"];
-    $cd=$_POST["cd"];
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $code=$_POST['code'];
 
-    mysql_connect("localhost","root","");
-    mysql_select_db("testing");
-    mysql_query("insert into test values('$nm','$em','$mb','$cd')");
+    $conn=new mysqli('sql211.ezyro.com','ezyro_27307703','b770l4x9fepd','ezyro_27307703_testing');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }
+    else{
+        $stmt=$conn->prepare("insert into test(name,email,mobile,code)
+        values(?,?,?,?)");
+        $stmt->bind_param("ssii",$name,$email,$mobile,$code);
+        $stmt->execute();
+        echo "sent successfully...";
+        $stmt->close();
+        $conn->close();
+    }
     ?>
 </body>
 </html>
